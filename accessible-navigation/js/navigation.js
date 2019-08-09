600831cdadcb2b34703fd01fc0cbb95842613c3a
Oliver Juhas <webmandesign@users.noreply.github.com>
2019-08-09 08:09:16 +0200

    Reverting back the `aria-expanded` attribute for menu container

--- accessible-navigation/js/navigation.js
+++ accessible-navigation/js/navigation.js
@@ -62,6 +62,7 @@
 		return;
 	}
 
+	menu.setAttribute( 'aria-expanded', 'false' );
 	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
 		menu.className += ' nav-menu';
 	}
@@ -70,9 +71,11 @@
 		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
 			container.className = container.className.replace( ' toggled', '' );
 			button.setAttribute( 'aria-expanded', 'false' );
+			menu.setAttribute( 'aria-expanded', 'false' );
 		} else {
 			container.className += ' toggled';
 			button.setAttribute( 'aria-expanded', 'true' );
+			menu.setAttribute( 'aria-expanded', 'true' );
 		}
 	};
 
